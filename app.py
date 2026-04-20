d308566ec09a2bfbbfe8571dd3041c47e34506e5
Maheshbunny123 <maheshgamedev@gmail.com>
2026-04-20 11:11:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -322,4 +322,5 @@ def get_stats():
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
